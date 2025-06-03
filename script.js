8aa7238ab84eef8d5a2378d6a420648c54a8dbcf
sakura-zen <zen2025pm@gmail.com>
2025-06-04 00:51:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,3 @@
 window.addEventListener('load', () => {
   alert("ようこそ、sakuraのポートフォリオへ！");
 });
-// マウスに合わせて表示
-document.addEventListener('mousemove', (e) => {
-  const petal = document.createElement('div');
-  petal.textContent = '🌸';
-  petal.style.position = 'absolute';
-  petal.style.left = `${e.pageX}px`;
-  petal.style.top = `${e.pageY}px`;
-  petal.style.pointerEvents = 'none';
-  petal.style.fontSize = '20px';
-  document.body.appendChild(petal);
-
-  setTimeout(() => {
-    petal.remove();
-  }, 1000);
-});
