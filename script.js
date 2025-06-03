244a2f7e382cc8f2c894fe209640b64003898c9e
sakura-zen <zen2025pm@gmail.com>
2025-06-04 00:48:28 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// マウスに合わせて表示
+document.addEventListener('mousemove', (e) => {
+  const petal = document.createElement('div');
+  petal.textContent = '🌸';
+  petal.style.position = 'absolute';
+  petal.style.left = `${e.pageX}px`;
+  petal.style.top = `${e.pageY}px`;
+  petal.style.pointerEvents = 'none';
+  petal.style.fontSize = '20px';
+  document.body.appendChild(petal);
+
+  setTimeout(() => {
+    petal.remove();
+  }, 1000);
+});
